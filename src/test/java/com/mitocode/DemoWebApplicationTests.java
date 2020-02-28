f008328a3f752de74f7832760de46e9d8152e584
mlopeza1 <mlopeza1@ucentral.edu.co>
2020-02-28 18:25:59 -0500

    primer commit ubuntu

--- src/test/java/com/mitocode/DemoWebApplicationTests.java
+++ src/test/java/com/mitocode/DemoWebApplicationTests.java
@@ -3,7 +3,7 @@ package com.mitocode;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,7 +13,7 @@ import com.mitocode.model.Usuario;
 import com.mitocode.repo.IUsuarioRepo;
 
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoWebApplicationTests {
 	
